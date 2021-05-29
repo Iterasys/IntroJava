b16a9b43521d810ca7a3bf3c140dcc5cfdf927a8
rafafcorreia <rafa.f.correia@gmail.com>
2021-05-29 13:14:47 -0300

    Código criado na 1ª aula

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,46 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Cálculo de Areas");
+
+        // Calcula de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + largura * comprimento + "m²");
+
+    }
+
+    public void calcularAreaModoExtenso(){
+        // Calculo de área - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // largura recebe 4
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + resultado + "m²");
+    }
+
+    public void calcularAreaModoCompacto(){
+        // Calcula de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + largura * comprimento + "m²");
+    }
+
+}
