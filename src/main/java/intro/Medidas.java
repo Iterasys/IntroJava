973412bdf8a3c79c90df58382593a272cb5d5bdb
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-06-05 10:47:31 -0300

    Inclui o "Bom dia!!" no começo da impressão de texto no Console

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,6 +9,7 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
+        System.out.println("Bom dia!!!");
         System.out.println("Cálculo de Areas");
 
         // Calcula de área - Reduzido
